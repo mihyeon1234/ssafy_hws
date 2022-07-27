3ed3f0a0c80fbb9d29cf0b7fe5c6b9fbbef0b893
이미현 <algus7531@naver.com>
2022-07-28 00:39:28 +0900

    숙제 중간커밋

--- 0726/practice/01_battleship.py
+++ 0726/practice/01_battleship.py
@@ -7,14 +7,15 @@ print("================================")
 
 # 필요에 따라 추가적으로 함수를 만들어 자유롭게 활용할 수 있습니다.
 # 각자의 해역에 배를 위치시키는 함수
-def set_ship(index, sea):
-    idx = int(input('배를 위치시킬 시작점을 고르세요. : '))
-    if idx >=1 and idx<=13:
-        (player_sea[idx] and player_sea[idx+1] and player_sea[idx+2])==1
-        print('*'*30)
-        print(f'플레이어의 해역 : {player_sea}')
-
-
+def set_ship(index):
+    player_sea[index-1]=1
+    player_sea[index]=1
+    player_sea[index+1]=1
+    print('-'*50)
+    print(f'플레이어의 해역 : {player_sea}')
+    print(f'플레이어의 공격 기록 : {player_attacked}')
+    print('-'*50)
+    
 
 player_sea = [0] * 15  # 플레이어의 해역
 player_attacked = [False] * 15  # 플레이어의 공격 위치 기록
@@ -26,14 +27,20 @@ round = 1  # 게임 라운드
 
 # 1. 게임 준비
 while True:
-    pass
-    # 1-1) 플레이어의 배 시작 위치 고르기
+    index = int(input('배를 위치시킬 시작점을 고르세요. : '))
 
+    # 1-1) 플레이어의 배 시작 위치 고르기
+    if index >=1 and index<=13:
+        set_ship(index)
+        break
     # 1-2) 범위를 벗어난 시작점을 고른 경우
+    else:
+        print('-----해당 위치에는 배를 둘 수 없습니다.-----')
 
 
 # 1-3) 컴퓨터의 배 시작 위치를 랜덤으로 지정합니다.
-
+a = list(range(1,14))
+coind = random.sample(a, 1)
 # 1-4) 플레이어와 컴퓨터의 해역에 각각 배 위치시키기
 
 # 2. 라운드 진행
